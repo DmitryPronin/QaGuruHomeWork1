15066d24a3d4e8ece23aff6fad72a14dfffcf7e1
dmitriypronin <dmit.pronin@gmail.com>
2021-12-20 21:39:29 +0300

    qaGuru: fix comments

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,19 +1,27 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit5.SoftAssertsExtension;
+import com.google.common.reflect.ClassPath;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 
 import java.io.File;
 
 import static com.codeborne.selenide.AssertionMode.SOFT;
+
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.*;
 
+@ExtendWith({SoftAssertsExtension.class})
 public class HomeWork {
-    private final SoftAssertions softAssert = new SoftAssertions();
 
     @BeforeAll
     static void setUp(){
@@ -33,35 +41,34 @@ public class HomeWork {
         String state = "NCR";
         String city = "Delhi";
         String subject = "Math";
-        File pict = new File("src/test/resources/files/picture.jpeg");
 
-        $(By.id("firstName")).setValue(firstname);
-        $(By.id("lastName")).setValue(lastName);
-        $(By.id("userEmail")).setValue(email);
-        $(By.xpath("//label[text()='Male']")).click();
-        $(By.id("userNumber")).scrollTo().setValue(mobileNumber);
-        $(By.id("dateOfBirthInput")).click();
-        $(By.className("react-datepicker__month-select")).selectOptionContainingText("February");
-        $(By.className("react-datepicker__year-select")).selectOptionContainingText("1988");
-        $(By.className("react-datepicker__day--027")).click();
-        $(By.xpath("//label[text()='Sports']")).click();
-        $(By.id("subjectsInput")).scrollTo().setValue(subject).pressEnter();
-        $(By.id("uploadPicture")).uploadFile(pict);
-        $(By.id("currentAddress")).setValue(address);
-        $(By.id("state")).click();
-        $(By.id("stateCity-wrapper")).$(byText(state)).click();
-        $(By.id("city")).click();
-        $(By.id("stateCity-wrapper")).$(byText(city)).click();
-        $(By.id("submit")).click();
+        $(id("firstName")).setValue(firstname);
+        $(id("lastName")).setValue(lastName);
+        $(id("userEmail")).setValue(email);
+        $(byText("Male")).click();
+        $(id("userNumber")).scrollTo().setValue(mobileNumber);
+        $(id("dateOfBirthInput")).click();
+        $(className("react-datepicker__month-select")).selectOptionContainingText("February");
+        $(className("react-datepicker__year-select")).selectOptionContainingText("1988");
+        $(className("react-datepicker__day--027")).click();
+        $(xpath("//label[text()='Sports']")).click();
+        $(id("subjectsInput")).scrollTo().setValue(subject).pressEnter();
+        $(id("uploadPicture")).uploadFromClasspath("files/picture.jpeg");
+        $(id("currentAddress")).setValue(address);
+        $(id("state")).click();
+        $(id("stateCity-wrapper")).$(byText(state)).click();
+        $(id("city")).click();
+        $(id("stateCity-wrapper")).$(byText(city)).click();
+        $(id("submit")).click();
 
-        softAssert.assertThat($(By.xpath("//td[text()='Student Name']/following-sibling::td")).getText()).contains(firstname + " " + lastName);
-        softAssert.assertThat($(By.xpath("//td[text()='Student Email']/following-sibling::td")).getText()).contains(email);
-        softAssert.assertThat($(By.xpath("//td[text()='Gender']/following-sibling::td")).getText()).contains("Male");
-        softAssert.assertThat($(By.xpath("//td[text()='Mobile']/following-sibling::td")).getText()).contains(mobileNumber);
-        softAssert.assertThat($(By.xpath("//td[text()='Date of Birth']/following-sibling::td")).getText()).contains(27 + " February," + 1988);
-        softAssert.assertThat($(By.xpath("//td[text()='Subjects']/following-sibling::td")).getText()).contains("Maths");
-        softAssert.assertThat($(By.xpath("//td[text()='Hobbies']/following-sibling::td")).getText()).contains("Sports");
-        softAssert.assertThat($(By.xpath("//td[text()='Picture']/following-sibling::td")).getText()).contains(pict.getName());
-        softAssert.assertThat($(By.xpath("//td[text()='State and City']/following-sibling::td")).getText()).contains(state + " " + city);
+        $(xpath("//td[text()='Student Name']/following-sibling::td")).shouldHave(text(firstname + " " + lastName));
+        $(xpath("//td[text()='Student Email']/following-sibling::td")).shouldHave(text(email));
+        $(xpath("//td[text()='Gender']/following-sibling::td")).shouldHave(text("Male"));
+        $(xpath("//td[text()='Mobile']/following-sibling::td")).shouldHave(text(mobileNumber));
+        $(xpath("//td[text()='Date of Birth']/following-sibling::td")).shouldHave(text(27 + " February," + 1988));
+        $(xpath("//td[text()='Subjects']/following-sibling::td")).shouldHave(text("Maths"));
+        $(xpath("//td[text()='Hobbies']/following-sibling::td")).shouldHave(text("Sports"));
+        $(xpath("//td[text()='Picture']/following-sibling::td")).shouldHave(text("picture.jpeg"));
+        $(xpath("//td[text()='State and City']/following-sibling::td")).shouldHave(text(state + " " + city));
     }
 }
